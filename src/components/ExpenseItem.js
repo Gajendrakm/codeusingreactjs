91554b74b93f7521d1dda852bf6830b6463f461a
Gajendrakm <gjendra@gmail.com>
2021-08-24 20:04:38 +0530

    reactready

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,13 @@
+function ExpenseItem() {
+    return (
+    <div>
+        <div>August 24th 2021</div>
+        <div>
+            <h2>Car Insurance</h2>
+            <div>8000Rupee</div>
+        </div>
+    </div>
+    );
+}
+
+export default ExpenseItem;
