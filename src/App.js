4901c29e735a582ff190668eba0843676d6c4df3
Gajendrakm <gjendra@gmail.com>
2021-08-27 23:11:26 +0530

    splitcomponent_to_multiplecomponent

--- src/App.js
+++ src/App.js
@@ -4,11 +4,11 @@ function App() {
     const expenses =[
         {id:'e1',title:'Toilet Paper',amount:94.12,date:new Date(2020,7,14)},
         
-        {id:'e2',title:'face wipe',amount:95.12,date:new Date(2020,8,14)},
+        {id:'e2',title:'Car Insurance',amount:95.12,date:new Date(2020,8,14)},
 
-        {id:'e3',title:'soft tissue',amount:96.12,date:new Date(2020,9,14)},
+        {id:'e3',title:'New TV',amount:96.12,date:new Date(2020,9,14)},
 
-        {id:'e4',title:'hand tissue',amount:97.12,date:new Date(2020,10,14)},
+        {id:'e4',title:'Desktop',amount:97.12,date:new Date(2020,10,14)},
     ];
     return (
         <div>
@@ -16,18 +16,23 @@ function App() {
             <ExpenseItem 
                 title={expenses[0].title}
                 amount={expenses[0].amount}
-                date={expenses[0].date}>
-            </ExpenseItem>
+                date={expenses[0].date}
+            />
             <ExpenseItem
                 title={expenses[1].title}
                 amount={expenses[1].amount}
-                date={expenses[1].date}>
-            </ExpenseItem>
+                date={expenses[1].date}
+            />
             <ExpenseItem
                 title={expenses[2].title}
                 amount={expenses[2].amount}
-                date={expenses[2].date}>
-            </ExpenseItem>
+                date={expenses[2].date}
+            />
+             <ExpenseItem
+                title={expenses[3].title}
+                amount={expenses[3].amount}
+                date={expenses[3].date}
+            />
         </div>
     );
 }
