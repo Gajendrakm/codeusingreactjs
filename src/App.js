d5f7a8db5ae42c9ff8656ecc7c1f9f270c91d4b7
Gajendrakm <gjendra@gmail.com>
2021-08-28 07:52:36 +0530

    composition-1

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import ExpenseItem from "./components/ExpenseItem";
+import Expenses from "./components/Expenses";
 
 function App() {
     const expenses =[
@@ -10,31 +10,14 @@ function App() {
 
         {id:'e4',title:'Desktop',amount:97.12,date:new Date(2020,10,14)},
     ];
+
     return (
         <div>
-            <h2>Let's get started!</h2>
-            <ExpenseItem 
-                title={expenses[0].title}
-                amount={expenses[0].amount}
-                date={expenses[0].date}
-            />
-            <ExpenseItem
-                title={expenses[1].title}
-                amount={expenses[1].amount}
-                date={expenses[1].date}
-            />
-            <ExpenseItem
-                title={expenses[2].title}
-                amount={expenses[2].amount}
-                date={expenses[2].date}
-            />
-             <ExpenseItem
-                title={expenses[3].title}
-                amount={expenses[3].amount}
-                date={expenses[3].date}
-            />
+            <h2>Lets get started</h2>
+            <Expenses items={expenses}/>
         </div>
-    );
+    )
+
 }
 
 export default App;
